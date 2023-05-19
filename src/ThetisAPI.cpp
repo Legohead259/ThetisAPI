b11b1f154d51a58e99076248a1a37effb4a69afc
Braidan <legohead259@gmail.com>
2023-05-19 17:31:35 -0400

    Updated some comments

--- src/ThetisAPI.cpp
+++ src/ThetisAPI.cpp
@@ -24,25 +24,43 @@ ThetisAPI api;
 
 
 // ==========================
-// === OVERLOAD FUNCTIONS ===
+// === OVERRIDE FUNCTIONS ===
 // ==========================
 
 
 /**
+ * The strobe command is send to the device to strobe the LED bright white for 5 seconds.
+ * The is "strobe" and the value is null.
+ * This command can be used to quickly find a specific device when using multiple devices.
+ * TODO: Make asynchronous (non-blocking)
+ * 
  * @override super::strobe()
 */
 void ThetisAPI::strobe() {
+    // TODO: Send acknowledgement message
     uint32_t startTime = millis();
     while((millis() - startTime) <= 5000) // Blink white for 5 seconds
         blink(250, WHITE);
 }
 
 /**
+ * The colour command is sent to the device to set the LED color.
+ * The key is "colour" and the value is either a Red Green Blue (RGB) hexidecimal triplet expressed as a string, or null.
+ * Setting the colour will override the default LED behavior.
+ * A value of null will restore the normal behavior.
+ * 
+ * @param colorString the hexadecimal string representation of the RGB color
  * @override super::color()
 */
 void ThetisAPI::colour(const char* colorString) {
+    if (colorString == nullptr) {
+        // TODO: Set the LED behavior flag back to NORMAL.
+    }
+
     Serial.println(colorString);
     uint32_t color = hexStringToUnsignedLong(colorString);
     Serial.println(color);
     setLEDColor(color);
+
+    // TODO: Send acknowledgement message
 }
